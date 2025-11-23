50913f56a4875b5f4a84a675425a641fde5b4eea
Alvin Heib <heibalvin@akashatek.com>
2025-11-23 17:01:34 +0400

    Initial Commit

---
+++ AH-CHR/AH_CHRApp.swift
@@ -0,0 +1,17 @@
+//
+//  AH_CHRApp.swift
+//  AH-CHR
+//
+//  Created by Alvin HEIB on 23/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AH_CHRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
